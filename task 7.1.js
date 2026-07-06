64c67c0c96523ebdd3c344a6fb4e0f055f010140
Vignesh448 <vigneshsubramani8@gmail.com>
2026-07-06 10:03:43 +0530

    task7.1

--- task 7.1.js
+++ task 7.1.js
@@ -1,175 +1,180 @@
+let employeeDetails = "https://6a475f5fabfcbaade1184ddb.mockapi.io/employeeDetails/EmployeeDetails";
 
-//⭐ Final Mini Projec
+let responseData = [];
 
-console.log("Final Mini Project");
+fetch(employeeDetails)
+    .then((response) => response.json())
+    .then((data) => {
+        responseData = data;
+        startProgram();
+    })
+    .catch((err) => console.log(err));
 
-//View Employees
-
-let Employees = [
-{
- name:"Naveen",
- id:101,
-salary:30000
-},
-{
- name:"John",
- id:102,
- salary:45000
-},
-{
- name:"Arun",
- id:103,
-salary:60000
-}
-];
-
-console.log("1.View Employees:-");
-
-for(let i=0; i<Employees.length; i++){
-    console.log(Employees[i]);
-    
-}
-
-//Search Employee by ID
-
-console.log("2.earch Employee by ID :-");
-
-let id = Number(prompt("Enter Employee ID"));
-for(let i =0; i<Employees.length; i++){
-    if(Employees[i].id==id){
-        console.log(Employees[i]);
-        
-    }
-}
-
-//Add Bonus
-
-console.log("3.Add Bonus(5000) :-");
-
-
-//Add Bonus
-
-
-for(let i=0; i<Employees.length; i++){
-    Employees[i].salary=Employees[i].salary+5000;
-   console.log(Employees[i]);
-}
-
-//Find highest salary
-console.log("4.highest Salary:-");
-
-let highest = Employees[0];
-
-for(let i=1; i<Employees.length; i++){
-    if(Employees[i].salary>highest.salary){
-        highest = Employees[i];
-    }
-}
-console.log(highest);
-
-//Find lowest salary.
-
-console.log("5.Lowest Salary :-");
-
-let lowest =Employees[0];
-for(let i=1; i<Employees.length; i++){
-    if(Employees[i].salary<lowest.salary){
-        lowest=Employees[i];
-    }
-}
-console.log(lowest);
-
-//Calculate Total Salary
-
-console.log("6.Total Salary :-");
-
-let totalSalary = 0;
-for(let i=0; i<Employees.length; i++){
- totalSalary = totalSalary + Employees[i].salary;
-}
-console.log(totalSalary);
-
-//Filter Salary Above ₹40,000
-
-console.log("7.Filter Salary Above ₹40,000 :-");
-
-function filtersalary(){
-    let result = Employees.filter(function(employee){
-        return employee.salary > 40000;
-    });
-console.log(result);
-}
-filtersalary();
-
-//Print Employee Report
-
-console.log("8.Employee Report : -");
-
-function employeeReport(){
-    Employees.forEach(function(employee){
-        console.log(`
-Employee ID : ${employee.id}
-Employee Name : ${employee.name}
-Employee Salary : ${employee.salary}
+function viewEmployee() {
+console.log("----- Employee Details -----");
+responseData.forEach((emp) => {
+console.log(`
+Employee Name : ${emp.name}
+Id            : ${emp.id}
+Salary        : ${emp.salary}
+Department    : ${emp.dept}
+Experience    : ${emp.experience}
 `);
     });
 }
-employeeReport();
 
-//Display Current Date & Time
+function searchById() {
+let find = Number(prompt("Enter Employee ID to Find"));
+let employee = responseData.find((emp)=> {
+  return emp.id === find;
+});
 
-console.log("9.Current Date & Time :-");
-
-function CurrentDate(){
-    console.log(new Date());
-    
+if (employee) {
+console.log(`Employee Search By Id.....
+Employee Name : ${employee.name}
+Id            : ${employee.id}
+Salary        : ${employee.salary}
+Department    : ${employee.dept}
+Experience    : ${employee.experience}
+`);
+    } else {
+        console.log("Employee Not Found");
+    }
 }
-CurrentDate();
 
-
-
-// Exit
-
-console.log("11.Exit :-");
-
-function Exit(){
-    console.log("THANK YOU");
-    
+function addBonus(){
+console.log("After adding the bonus.....");
+responseData.forEach((emp) => {
+let totalSalary = emp.salary + emp.bonus;
+console.log(`
+Employee Name : ${emp.name}
+Id            : ${emp.id}
+Salary        : ${totalSalary}
+Department    : ${emp.dept}
+Experience    : ${emp.experience}
+`);
+});
 }
-Exit();
 
-
-
-//Fetch Employee Data from an API
-
-console.log("10.Fetch Employee Data from API :-");
-
-function fetchEmployee() {
-    fetch("https://dummyjson.com/users").then(function(response) {
-            return response.json();
-        })
-    .then(function(data) {
-         data.users.forEach(function(employee) {
-                console.log(`
-Employee ID : ${employee.id}
-Employee Name : ${employee.firstName} ${employee.lastName}
-Employee Email : ${employee.email}
-                `);
-            });
-        })
-        .catch(function(error) {
-            console.log(error);
-        });
+function highestSal(){
+let highestSalary = responseData[0].salary;
+responseData.forEach((emp)=>{
+if (emp.salary > highestSalary) {
+highestSalary = emp.salary;
+  }
+  })  
+console.log(`Highest Salary : ${highestSalary}`);
 }
-fetchEmployee();
+
+function lowestSal(){
+let lowestSalary = responseData[0].salary;
+responseData.forEach((emp)=>{
+if (emp.salary < lowestSalary) {
+lowestSalary = emp.salary;
+  }
+  }) 
+console.log(`Lowest Salary : ${lowestSalary}`);
+}
+
+function totalSal(){
+    let total = responseData.reduce((acc,emp)=>{
+        return acc + emp.salary
+    },0)
+    console.log(`Total Salary : ${total}`);
+}
+
+function filterEmp() {
+let details = responseData.filter((emp) => {
+    return emp.salary > 40000;
+});
+console.log("----- Salary Above 40,000 -----");
+details.forEach((emp) => {
+console.log(`
+Employee Name : ${emp.name}
+Id            : ${emp.id}
+Salary        : ${emp.salary}
+Department    : ${emp.dept}
+Experience    : ${emp.experience}
+`);
+});
+}
 
 
+function dateAndTime(){
+let currentDateTime = new Date();
+let date = `${currentDateTime.getDate()}-${currentDateTime.getMonth() + 1}-${currentDateTime.getFullYear()}`;
+let time = `${currentDateTime.getHours()}:${currentDateTime.getMinutes()}:${currentDateTime.getSeconds()}`;
 
- 
+console.log(`
+Current Date : ${date}
+Current Time : ${time}
+`);
+}
 
+function startProgram() {
 
- 
+    let choice;
 
+    do {
 
+        choice = prompt(`
+===== Employee Management System =====
 
+1. View Employees
+2. Search Employee By ID
+3. Calculate Bonus
+4. Find Highest Salary
+5. Find Lowest Salary
+6. Calculate Total Salary
+7. Filter Salary Above ₹40,000
+8. Display Current Date & Time
+9. Exit
 
+Enter Your Choice
+`);
+
+        switch (choice) {
+
+            case "1":
+                viewEmployee();
+                break;
+
+            case "2":
+                searchById();
+                break;
+
+            case "3":
+                addBonus();
+                break;
+
+            case "4":
+                highestSal();
+                break;
+
+            case "5":
+                lowestSal();
+                break;
+
+            case "6":
+                totalSal();
+                break;
+
+            case "7":
+                filterEmp();
+                break;
+
+            case "8":
+                dateAndTime();
+                break;
+
+            case "9":
+                alert("Thank You!");
+                break;
+
+            default:
+                alert("Invalid Choice");
+        }
+
+    } while (choice !== "9");
+
+}
